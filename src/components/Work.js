1be65ae68b246d0b6e0b545fa555949c3de98009
Andreea Gardelean <andreea.gardelean.2020@live.rhul.ac.uk>
2023-12-15 19:57:09 +0000

    - css updates

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,7 +3,27 @@ import '../css/work.css';
 const Work = () => {
   return (
     <div id="work">
-      <p> My work: </p>
+      <div id="work-contents">
+        <div id="map">
+          <p>Offline Map</p>
+        </div>
+
+        <div id="to-do">
+          <p>Offline To-Do application</p>
+        </div>
+
+        <div id="restaurant">
+          <p>Restaurant application</p>
+        </div>
+
+        <div id="Calculator">
+          <p>Calculator</p>
+        </div>
+
+        <div id="App">
+          <p>App</p>
+        </div>
+      </div>
     </div>
   )
 }
