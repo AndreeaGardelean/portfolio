b4812dffff67081dad60f2668a4a5b4373feb0b7
Andreea Gardelean <andreea.gardelean.2020@live.rhul.ac.uk>
2024-11-28 14:32:03 +0000

    Updated welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,16 @@
 import React from "react";
-import '../css/welcome.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import "../css/welcome.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 import redirect from "../shared/redirect";
 
 const Welcome = () => {
   return (
-    <div className='intro' id="welcome">
+    <div className="intro" id="welcome">
       <article className="description">
         <p className="greeting">Hi, my name is</p>
         <p className="name">Andreea Gardelean</p>
-        <p className="phrase">Computer Science Graduate, Software Engineer & Full Stack Developer</p>
+        <p className="phrase">Software Engineer, Computer Science Graduate</p>
       </article>
 
       <article className="more" onClick={() => redirect("about")}>
@@ -18,7 +18,7 @@ const Welcome = () => {
         <FontAwesomeIcon icon={faAngleDown} id="icon" />
       </article>
     </div>
-  )
-}
+  );
+};
 
 export default Welcome;
